096cf1ee0bd75d03e70501c94331a1751c0598f3
JRReduta <jrreduta@yahoo.com>
2025-08-06 16:10:37 -0700

    Wrote & tested api/notes.js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 import app from "#app";
+import { addNote, getNoteById, getNotes } from "#db/notes";
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
+});
+
+app.get("/", (req, res) => {
+  addNote("baba booey");
+  res.status(200).send(getNotes());
 });
