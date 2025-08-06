41786524e8949bb8579143e592739ed70779411f
JRReduta <jrreduta@yahoo.com>
2025-08-06 16:39:54 -0700

    Added body-parsing middleware, added and tested notes/ routing

--- api/notes.js
+++ api/notes.js
@@ -7,6 +7,12 @@ router.get("/", (req, res) => {
   res.status(200).send(getNotes());
 });
 
+router.post("/", (req, res, next) => {
+  console.log(req.body);
+
+  res.status(404).send(req.body);
+});
+
 router.get("/:id", (req, res) => {
   res.status(200).send("TODO: /notes/:id");
 });
